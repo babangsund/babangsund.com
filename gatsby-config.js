d82592b03cb8965876bf62ac2f165836fc075738
babangsund <52657398+babangsund@users.noreply.github.com>
2019-07-08 23:33:15 +0200

    Create now.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,13 +26,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
