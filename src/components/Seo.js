216df2631385c468347e0e9645f88de887c868f9
babangsund <babangsund@gmail.com>
2019-09-01 21:50:25 -0100

    add base url to og:image; summary_large_image for twitter card

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -37,8 +37,8 @@ function SEO({ url = "", title = "", description = "" }) {
       <meta property="og:title" content={metaTitle} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content={bab_image} />
-      <meta name="twitter:card" content="summary" />
+      <meta property="og:image" content={site.siteMetadata.url + bab_image} />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={site.siteMetadata.twitter} />
       <meta name="twitter:title" content={metaTitle} />
       <meta name="twitter:description" content={metaDescription} />
