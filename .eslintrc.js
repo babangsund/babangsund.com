77de901afdb572da1480f874df7282f53517b6d5
babangsund <babangsund@gmail.com>
2019-11-11 23:13:22 +0100

    update prettier/eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
+  plugins: ['react', 'react-hooks'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  extends: 'react-app',
+  plugins: ['react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+  },
   globals: {
     graphql: false,
     __PATH_PREFIX__: true,
   },
-  extends: 'react-app',
-  plugins: ['react-hooks'],
 };
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: false,
+};
